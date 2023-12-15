c7913f38cf3197b35f4cdb43f78b888781e6db33
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-15 12:36:31 -0300

    Propriedades read only

---
+++ Modulo2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Modulo2.models;
+
+Pessoa p1 = new Pessoa();
+p1.Idade = 22;
+p1.Nome = "Rafael";
+p1.Sobrenome = "Silva";
+p1.Apresentar();
---
+++ Modulo2/models/Pessoa.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modulo2.models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+        public string Nome 
+        {
+            get => _nome.ToUpper();    
+        
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+
+
+        public string Sobrenome { get; set; }
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToLower();
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Idade não pode ser menor que zero");
+                }
+
+                _idade = value;
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
