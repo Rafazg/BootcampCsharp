f7b96247baf3875abda4f99086006037fcb63f68
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-16 21:35:19 -0300

    Atualização para projeto C# e tratamento de exceções.

--- LeitorDeArquivos/Program.cs
+++ LeitorDeArquivos/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System;
+using System.IO;
 try
 {
 
@@ -11,7 +13,16 @@ foreach (string linha in linhas)
     Console.WriteLine(linha);
 }
 
-} catch(Exception ex)
+}
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não econtrado. {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Caminho da pasta não encontrado. {ex.Message}");
+}
+catch(Exception ex)
 {
     Console.WriteLine($"Ocorreu uma exeção genérica. {ex.Message}");
 }
