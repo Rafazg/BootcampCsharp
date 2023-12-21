0066b9200f94004c1e385320cc4058d5957f9a63
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-21 20:56:11 -0300

    Classe de Teste

---
+++ ModuloDeTeste/CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,24 @@
+using Calculadora.Services;
+
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTestes()
+    {
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        // Arrange
+
+        // Act
+        //Assert
+
+    }
+}
